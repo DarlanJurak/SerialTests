8367d4feaf3da79a83e7541d4771272d88c87503
Darlan <darlan.jurak@gmail.com>
2017-10-16 15:01:21 -0200

    Included libraries

--- src/SerialTest.cpp
+++ src/SerialTest.cpp
@@ -5,12 +5,16 @@
 */
 
 #include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <typeinfo>
 #include <termios.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+
 #include <string.h>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -18,26 +22,26 @@
 using namespace cv;
 using namespace std;
 
-int main( int argc, char** argv[] )
+int main(int argc, char* argv[])
 {
     
     int fd = open(argv[1], O_RDWR);
 	if (fd == -1) {
-	  perror("%s", argv[1]);
+	  perror(argv[1]);
 	  return 1;
 	}
 
-	struct termios tios;
-	tcgetattr(fd, &tios);
-	// disable flow control and all that, and ignore break and parity errors
-	tios.c_iflag = IGNBRK | IGNPAR;
-	tios.c_oflag = 0;
-	tios.c_lflag = 0;
-	cfsetspeed(&tios, B9600);
-	tcsetattr(fd, TCSAFLUSH, &tios);
+	// struct termios tios;
+	// tcgetattr(fd, &tios);
+	// // disable flow control and all that, and ignore break and parity errors
+	// tios.c_iflag = IGNBRK | IGNPAR;
+	// tios.c_oflag = 0;
+	// tios.c_lflag = 0;
+	// cfsetspeed(&tios, B9600);
+	// tcsetattr(fd, TCSAFLUSH, &tios);
 
-	// the serial port has a brief glitch once we turn it on which generates a
-	// start bit; sleep for 1ms to let it settle
+	// // the serial port has a brief glitch once we turn it on which generates a
+	// // start bit; sleep for 1ms to let it settle
 	usleep(1000);    
 
 	// output to serial port
